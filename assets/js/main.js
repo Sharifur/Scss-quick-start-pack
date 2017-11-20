2ba1ce2f324fc1275060237ddfb2b4f40f789205
Sharifur Rahman <robinmedia7@gmail.com>
2017-11-20 18:09:06 +0600

    Uploder
    Quick Start pack for Scss

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"use strict";
+/*--document ready functions--*/
+    jQuery(document).ready(function($){
+
+    });
+
+/*--window Scroll functions--*/
+    $(window).on('scroll', function () {
+            
+    });
+           
+/*--window load functions--*/
+    $(window).on('load',function(){
+		/*
+        var preLoder = $(".preloader");
+        preLoder.fadeOut(1000);
+		*/
+    });
+
+}(jQuery));	
